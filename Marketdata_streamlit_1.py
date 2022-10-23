39bcfa765b8fb1eb59439584acdbe5c189db4ed2
buerbaumer <44548809+buerbaumer@users.noreply.github.com>
2022-10-23 08:54:36 +0200

    Update Marketdata_streamlit_1.py

--- Marketdata_streamlit_1.py
+++ Marketdata_streamlit_1.py
@@ -14,7 +14,8 @@ tickerSymbol = st.text_input("Ticker Symbol:", tickerSymbol_def)
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2022-10-23')
+#tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2022-10-23')
+tickerDf = tickerData.history(period='max')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.write("""
